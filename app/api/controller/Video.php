d85ba77c1c3355de8dc7cfdab1f78e27bd7fe4c0
lsiten <lsiten@Eric-mini.local>
2017-11-30 09:59:37 +0800

    dev

--- app/api/controller/Video.php
+++ app/api/controller/Video.php
@@ -121,7 +121,7 @@ class Video extends Base{
         $video =  new VideoModel();
         $creationModel = new Creation();
         $videoData = $video->where(["qiniu_key"=>$qiniu_key])->find()->toArray();   
-        if(!$videoData["audio_public_id"] || $videoData["qiniu_final_key"] || !$videoData["qiniu_final_poster"])
+        if(!$videoData["audio_public_id"] || !$videoData["qiniu_final_key"] || !$videoData["qiniu_final_poster"])
         {
             $this->return["code"] = 4031;
             $this->return["success"] = false;
