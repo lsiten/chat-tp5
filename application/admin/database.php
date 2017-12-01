e65b4714b906091c05796e63b01742fe1237458e
lsiten <lsiten@Eric-mini.local>
2017-12-01 18:57:23 +0800

    dev

--- application/admin/database.php
+++ application/admin/database.php
@@ -9,7 +9,7 @@ return [
     // 用户名
     'username'       => 'root',
     // 密码
-    'password'       => 'root',
+    'password'       => '2012.lei',
     // 端口
     'hostport'       => '',
     // 连接dsn
@@ -19,7 +19,7 @@ return [
     // 数据库编码默认采用utf8
     'charset'        => 'utf8',
     // 数据库表前缀
-    'prefix'         => 'snake_',
+    'prefix'         => 'lsiten_',
     // 数据库调试模式
     'debug'          => true,
     // 数据库部署方式:0 集中式(单一服务器),1 分布式(主从服务器)
